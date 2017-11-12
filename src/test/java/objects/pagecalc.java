6b3894a3c36dea2d193fb8de58f826ba62e6b285
Anastasiya <amartynova1809@gmail.com>
2017-11-12 20:14:33 +0200

    add allure annotations

--- src/test/java/objects/pagecalc.java
+++ src/test/java/objects/pagecalc.java
@@ -2,7 +2,7 @@ package objects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
+import ru.yandex.qatools.allure.annotations.Step;
 import java.io.File;
 
 public class pagecalc {
@@ -17,6 +17,7 @@ public class pagecalc {
         this.driver = driver;
     }
 
+    @Step
     public String calculate(String expression) {
         for (int i = 0; i < expression.length(); i++) {
             driver.findElement(By.xpath(String.format(button, expression.charAt(i)))).click();
@@ -26,12 +27,14 @@ public class pagecalc {
         return driver.findElement(resultBox).getAttribute("value");
     }
 
+    @Step
     public void open() {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("calc.html").getFile());
         driver.get("file:///" + file.getAbsolutePath());
     }
 
+    @Step
     public void clear() {
         driver.findElement(clean).click();
     }
--- src/test/java/tests/Maincalc.java
+++ src/test/java/tests/Maincalc.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import ru.yandex.qatools.allure.annotations.Features;
+import ru.yandex.qatools.allure.annotations.Stories;
 
 public class Maincalc extends Basecalc {
 
@@ -21,23 +23,33 @@ public class Maincalc extends Basecalc {
     }
 
     @Test
+    @Stories("Add story")
+    @Features("Calculator")
     public void add_test() {
         Assert.assertEquals("141", calc.calculate("85+56"));
     }
 
     @Test
+    @Stories("Sub story")
+    @Features("Calculator")
     public void sub_test() {
         Assert.assertEquals("35", calc.calculate("56-21"));
     }
 
     @Test
+    @Stories("Mult story")
+    @Features("Calculator")
     public void mult_test() {
         Assert.assertEquals("492", calc.calculate("41x12"));
     }
 
     @Test
+    @Stories("Div story")
+    @Features("Calculator")
     public void div_test() {
         Assert.assertEquals("3", calc.calculate("99/33"));
     }
+
+
 
 }
